df03ffe382def36c9c2261adbda0bc6975710387
marbellymoreno <me0763032023@unab.edu.sv>
2025-03-07 01:20:55 -0600

    Guía 7: Endpoint para login

---
+++ WebAPI/Controllers/ProfesorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reactBackend.Models;
+using reactBackend.Repository;
+
+namespace WebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+        private ProfesorDAO _proDAO = new ProfesorDAO();
+        [HttpGet("autentificacion")]
+
+        public string loginProfesor([FromBody] Profesor prof)
+        {
+            var prof1 = _proDAO.login(prof.Usuario, prof.Password);
+            if (prof1 != null)
+            {
+                return prof1.Usuario;
+            }
+            else
+            {
+                return "Usuario o contraseña incorrectos";
+            }
+        }
+    }
+}
---
+++ reactBackend/Repository/ProfesorDAO.cs
@@ -0,0 +1,25 @@
+﻿using reactBackend.Context;
+using reactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactBackend.Repository
+{
+    public class ProfesorDAO
+    {
+        #region Context
+        public RegistroAlumnosContext context = new RegistroAlumnosContext();
+        #endregion
+
+        public Profesor login(string usuario, string pass)
+        {
+            var prof = context.Profesors.Where(p => p.Usuario == usuario && p.Password == pass).FirstOrDefault();
+
+            return prof;
+        }
+    }
+}
