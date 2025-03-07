528b227ffa2ef150c04d223ccce159df61d4c376
marbellymoreno <me0763032023@unab.edu.sv>
2025-03-06 23:56:24 -0600

    Guia 4

--- reactBackend/Repository/AlumnoDAO.cs
+++ reactBackend/Repository/AlumnoDAO.cs
@@ -10,26 +10,97 @@ namespace reactBackend.Repository
 {
     public class AlumnoDAO
     {
-
         #region Context
-        public RegistroAlumnosContext contexto = new RegistroAlumnosContext();
+        public RegistroAlumnosContext contecxto = new RegistroAlumnosContext();
         #endregion
 
         #region SelectAll
         public List<Alumno> SelectAll()
         {
-            var alumno = contexto.Alumnos.ToList<Alumno>();
+            var alumno = contecxto.Alumnos.ToList<Alumno>();
             return alumno;
         }
         #endregion
 
-        #region SelectByID
-        public Alumno? GetByID(int id)
+        #region GetByID
+        public Alumno GetByID(int id)
         {
-            var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            var alumno = contecxto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
             return alumno == null ? null : alumno;
         }
         #endregion
 
+        #region InsertAlumno
+        public bool InsertAlumno(Alumno alumno)
+        {
+            try
+            {
+                var alum = new Alumno()
+                {
+                    Nombre = alumno.Nombre,
+                    Dni = alumno.Dni,
+                    Direccion = alumno.Direccion,
+                    Email = alumno.Email
+                };
+
+                contecxto.Alumnos.Add(alum);
+                contecxto.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region UpdateAlumno
+        public bool UpdateAlumno(int id, Alumno alumno)
+        {
+            try
+            {
+                var alumnoUpdate = GetByID(id);
+                if (alumnoUpdate == null)
+                {
+                    return false;
+                }
+
+                alumnoUpdate.Nombre = alumno.Nombre;
+                alumnoUpdate.Dni = alumno.Dni;
+                alumnoUpdate.Direccion = alumno.Direccion;
+                alumnoUpdate.Email = alumno.Email;
+                contecxto.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                return false;
+            }
+        }
+        #endregion
+
+        #region DeleteAlumno
+        public bool DeleteAlumno(int id)
+        {
+            try
+            {
+                var alumno = GetByID(id);
+                if (alumno == null)
+                {
+                    return false;
+                }
+
+                contecxto.Alumnos.Remove(alumno);
+                contecxto.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                return false;
+            }
+        }
+        #endregion
     }
 }
